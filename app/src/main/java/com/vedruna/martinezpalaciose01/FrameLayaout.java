38f1f4c3c9bd8eab290ec8cf73b9f224c7030f4a
ChemaDvp <chemamartineztkd@gmail.com>
2024-01-11 19:15:47 +0100

    .

---
+++ app/src/main/java/com/vedruna/martinezpalaciose01/FrameLayaout.java
@@ -0,0 +1,18 @@
+package com.vedruna.martinezpalaciose01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class FrameLayaout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frame_layaout);
+    }
+
+    
+}
--- app/src/main/java/com/vedruna/martinezpalaciose01/MainActivity.java
+++ app/src/main/java/com/vedruna/martinezpalaciose01/MainActivity.java
@@ -1,8 +1,12 @@
 package com.vedruna.martinezpalaciose01;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
