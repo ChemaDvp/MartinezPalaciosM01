dbf21a3edb2c9abbdd0caca79feeb21f00007aaa
ChemaDvp <chemamartineztkd@gmail.com>
2024-01-11 20:38:58 +0100

    ultima version

--- app/src/main/java/com/vedruna/martinezpalaciose01/FrameLayaout.java
+++ app/src/main/java/com/vedruna/martinezpalaciose01/FrameLayaout.java
@@ -1,9 +1,12 @@
 package com.vedruna.martinezpalaciose01;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
 public class FrameLayaout extends AppCompatActivity {
@@ -12,7 +15,22 @@ public class FrameLayaout extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frame_layaout);
-    }
 
-    
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setSelectedItemId(R.id.fragment_Uno);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            if (item.getItemId() == R.id.fragment_Uno) {
+                navController.navigate(R.id.fragment_Uno);
+            } else if (item.getItemId() == R.id.fragment_Dos) {
+                navController.navigate(R.id.fragment_Dos);
+            }
+            return true;
+        });
+
+    }
 }
